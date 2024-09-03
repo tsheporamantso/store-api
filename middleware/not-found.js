0922b53e7243799fbaee060518d854f338206184
tsheporamantso <tshepo.ramantso@outlook.com>
2024-09-03 08:30:13 +0200

    Delete all working files and folders

--- middleware/not-found.js
+++
@@ -1,5 +0,0 @@
-const notFound = (req, res) => {
-  res.status(404).send('<h1>Route Doesn Not Exist⛔</h1>');
-};
-
-module.exports = notFound;
--- models/product.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ProductSchema = new mongoose.Schema({
-  product: {
-    types: String,
-  },
-});
-
-module.exports = mongoose.model('Product', ProductSchema);
