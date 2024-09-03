bd0c50285df9f4b7e371ea657f5009c9018561e6
tsheporamantso <tshepo.ramantso@outlook.com>
2024-09-03 10:34:13 +0200

    Populate data dynamically to Database

---
+++ populate.js
@@ -0,0 +1,18 @@
+const connectDB = require('./db/connect');
+const jsonProducts = require('./populate.json');
+const Product = require('./models/product');
+require('dotenv').config();
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    await Product.create(jsonProducts);
+    console.log('Success');
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+start();
