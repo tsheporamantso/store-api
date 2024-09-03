26a65e00d80ae6ce407e226189c6a4cc5ec0ff6b
tsheporamantso <tshepo.ramantso@outlook.com>
2024-09-03 09:17:43 +0200

    Import routes

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const products = require('./routes/products');
+const notFoundMiddleware = require('./middleware/not-found');
 require('dotenv').config();
 
 const app = express();
 
+// Middleware
+app.use(express.json());
+
+// Routes
 app.get('/', (req, res) => {
   res
     .status(200)
     .send('<h1>STORE API🛍️</h1><a href="/api/v1/products">Products Route</a>');
 });
+app.use('/api/v1/products', products);
+
+// products routes
+app.use(notFoundMiddleware);
 
 const PORT = process.env.PORT || 3000;
 
