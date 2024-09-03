b15804087de405167105eed8002d0788a05be63b
tsheporamantso <tshepo.ramantso@outlook.com>
2024-09-03 12:13:45 +0200

    Add delete many method before creating data

--- populate.js
+++ populate.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
+    await Product.deleteMany();
     await Product.create(jsonProducts);
     console.log('Success');
     process.exit(0);
